6f99e82e8e135ff16e2229f2edfaf1fdb2b8ed28
Ilya <62900479+weltkind6@users.noreply.github.com>
2020-11-14 10:15:51 +0300

    avatars render

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import classes from './Dialogs.module.css'
-import ava1 from '../../img/ava1.png'
-import ava2 from '../../img/freckle.png'
-import ava3 from '../../img/blue.jpg'
+
 import Messages from "./Messages/Messages";
 import Dialogsitem from "./DialogsItem/Dialogsitem";
 
@@ -11,7 +9,7 @@ const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogsData.map(d =>
         <div className={classes.user_block}>
-            <img src={ava1} alt="avatar" className={classes.ava}/>
+            <Avatars ava={d.img}/>
             <Dialogsitem name={d.name} id={d.id}/>
         </div>)
 
@@ -23,7 +21,7 @@ const Dialogs = (props) => {
                 <div className={classes.users}>
                     {dialogsElements}
                 </div>
-                <div className={classes.messages}>
+                <div>
                     {messageContainer}
                 </div>
             </div>
@@ -33,14 +31,13 @@ const Dialogs = (props) => {
     )
 }
 
-
+const Avatars = (props) => {
+    return <div>{props.ava}</div>
+}
 
 
 // Переменная path для сокращения
 // Так сливается путь и айдишник /messages/ + props.id
 
-/*const Messages = (props) => {
-    return <div className={classes.users_message}>{props.message}</div>
-}*/
 
 export default Dialogs
--- src/Components/Dialogs/Messages/Messages.js
+++ src/Components/Dialogs/Messages/Messages.js
@@ -3,7 +3,9 @@ import classes from './Messages.module.css'
 
 
 const Messages = (props) => {
-    return <div className={classes.users_message}>{props.message}</div>
+    return (<div className={classes.users_wrapper}>
+        <div className={classes.users_message}>{props.message}</div>
+    </div>)
 }
 
 export default Messages

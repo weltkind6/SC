114dabe8d7eb3e799422852b15a68645dd3515f9
Ilya <62900479+weltkind6@users.noreply.github.com>
2020-12-10 19:05:04 +0300

    #52 add-users

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,35 +1,26 @@
 import React from 'react'
 import mage from '../../img/mage.png'
 import classes from './Users.module.css'
+import * as axios from 'axios'
 
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                followed: false,
-                fullName: 'Ilay',
-                status: 'Have job',
-                location: {city: 'Prague', country: 'Czech Republic'},
-            },
-            {
-                id: 2,
-                followed: true,
-                fullName: 'Vasdyua',
-                status: 'Freelance',
-                location: {city: 'Moscow', country: 'Russia Federation'}
-            },
-            {id: 3, followed: true, fullName: 'Herojivs', status: 'Rester', location: {city: 'Stalingrad', country: 'USSR'}}
 
-        ])
+    let gertUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                debugger
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={gertUsers}>Get users</button>
             {props.users.map(u => <div key={u.id}>
                 <div>
-                    <div><img src={mage} alt="mage" className={classes.avatar}/></div>
+                    <div><img src={u.photos.small != null ? u.photos.small : mage} className={classes.avatar}/></div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
@@ -41,10 +32,10 @@ const Users = (props) => {
                     </div>
                 </div>
                 <div>
-                    <div>{u.fullName}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
-                    <div>{u.location.city}</div>
-                    <div>{u.location.country}</div>
+                    <div>{}</div>
+                    <div>{}</div>
                 </div>
             </div>)}
         </div>

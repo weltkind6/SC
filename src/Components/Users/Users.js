9cbcdb668804feb4669f8b28c5553f427776a99d
Ilya <62900479+weltkind6@users.noreply.github.com>
2020-12-24 10:26:31 +0300

    62 fixed AJAX

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from "./Users.module.css";
 import mage from "../../img/mage.png";
 import {NavLink} from "react-router-dom";
+import * as axios from 'axios'
 
 
 const Users = (props) => {
@@ -13,33 +14,38 @@ const Users = (props) => {
     return (
         <div className={classes.users_wrapper}>
             <div className={classes.pages_block}>
-                {pages.map((p) => <span onClick={() => {
-                    props.onPageChanged(p)
-                }} className={props.currentPage === p && classes.active}>{p}</span>)}
+                {pages.map((p) => {
+                    return <span className={props.currentPage === p && classes.active}
+                                 onClick={(e) => {
+                                     props.onPageChanged(p)
+                                 }}>{p}</span>
+                })}
             </div>
+
+
             {props.users.map(u => <div key={u.id}>
-                    <div className={classes.users_block}>
-                        <div>
-                            <NavLink to={'/profile/' + u.id}>
-                                <img alt='' src={u.photos.small != null ? u.photos.small : mage}
-                                     className={classes.avatar}/>
-                            </NavLink>
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button className={classes.user_button} onClick={() => {
-                                    props.unfollow(u.id)
-                                }}>UnFollow</button> :
-                                <button className={classes.user_button} onClick={() => {
-                                    props.follow(u.id)
-                                }}>Follow</button>}
-                        </div>
+                <div className={classes.users_block}>
+                    <div>
+                        <NavLink to={'/profile/' + u.id}>
+                            <img alt='' src={u.photos.small != null ? u.photos.small : mage}
+                                 className={classes.avatar}/>
+                        </NavLink>
                     </div>
                     <div>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
+                        {u.followed
+                            ? <button className={classes.user_button} onClick={() => {
+                                props.unfollow(u.id)
+                            }}>UnFollow</button> :
+                            <button className={classes.user_button} onClick={() => {
+                                props.follow(u.id)
+                            }}>Follow</button>}
                     </div>
-                </div>)
+                </div>
+                <div>
+                    <div>{u.name}</div>
+                    <div>{u.status}</div>
+                </div>
+            </div>)
             }</div>
     )
 }

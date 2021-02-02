072371aa5101e69885c320eab0ffdd9dde0b4cec
Ilya <62900479+weltkind6@users.noreply.github.com>
2021-02-02 21:49:15 +0300

    #51 users API

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,28 +1,14 @@
 import React from 'react'
+import axios from "axios"
+import noava from '../../img/noava.svg'
+
 
 const Users = props => {
+
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {follow: true, fullName: 'Dmitriy', id: 1, status: 'Boss', location: {city: 'Minsk', country: 'Belarus'}},
-                {follow: true, fullName: 'Katuha', id: 2, status: 'Friend', location: {city: 'Moscow', country: 'Russia'}},
-                {
-                    follow: false,
-                    fullName: 'Artyom',
-                    id: 3,
-                    status: 'Worker',
-                    location: {city: 'Prague', country: 'Czech-Republic'}
-                },
-                {follow: true, fullName: 'Vikky', id: 4, status: 'Driver', location: {city: 'Paris', country: 'France'}},
-                {
-                    follow: false,
-                    fullName: 'Santey',
-                    id: 5,
-                    status: 'Director',
-                    location: {city: 'Geneva', country: 'Switzerland '}
-                },
-            ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        })
     }
 
     return (
@@ -31,9 +17,9 @@ const Users = props => {
             <div>
                 {props.users.map(u =>
                     <div>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
-                        <div>{u.location.city}</div>
+                        <div>{u.name}</div>
+                        <div>{u.id}</div>
+                        <img src={u.photos.small != null ? u.photos.small : noava} alt=""/>
                         <div>
                             {u.follow ?
                                 <button onClick={() => props.goUnfollow(u.id)}>Unfollow</button> :

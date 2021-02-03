f1701fc259f6d9df350d78914f1a4ec66086cb42
Ilya <62900479+weltkind6@users.noreply.github.com>
2021-02-03 11:30:30 +0300

    #54 componentDidMount

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -7,8 +7,12 @@ import classes from './Users.module.css'
 class Users extends React.Component {
     constructor(props) {
         super(props);
+
+    }
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
+            this.props.setUsers(response.data.items)
         })
     }
 
